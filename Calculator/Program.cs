6ae9a8324f4b4a4477c80f02e84c6d18ac265700
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2023-11-03 22:01:23 +0100

    Add basic math functions
    - Addition function
    
    - Subtraction function
    
    - Division function
    
    - Multiplication function

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -95,5 +95,106 @@ namespace Calculator
 			result = Array.ConvertAll(numbers.Split(","), int.Parse);
 			return result;
 		}
+		
+		/// <summary>
+		/// Calculate the sum and show the result in console window
+		/// </summary>
+		static void Addition()
+		{
+			// Show a header of this addition function
+			ShowAHeader("Addition");
+
+			// Variables for calculation of the sum
+			int result = 0;
+			int[] numbers = GetSomeInterger();
+
+			// Calculate the sum and show the result in console window
+			Console.Write("The sum of ");
+			for(int x = 0; x < numbers.Length; x++) 
+			{
+				// Add value to result
+				result += numbers[x];
+
+				// Show the value added to result for the user
+				Console.Write(numbers[x]);
+				if (x < numbers.Length - 1)
+					Console.Write(", ");
+			}
+			Console.WriteLine($" are {result}.");
+		}
+		/// <summary>
+		/// Calculate the difference and show the result in console window
+		/// </summary>
+		static void Subtraction()
+		{
+			// Show a header of this subtraction function
+			ShowAHeader("Subtraction");
+
+			// Variables for calculation of difference
+			int result = 0;
+			int[] numbers = GetSomeInterger();
+
+			// Calculate the difference and show the result in console window
+			Console.Write("The difference from ");
+			for (int x = 0; x < numbers.Length; x++)
+			{
+				// Check if the number are 1:st one, then set or reduce value to result
+				if (x == 0)
+					result = numbers[x];
+				else
+					result -= numbers[x];
+
+				// Show the value that set or reduce result
+				Console.Write(numbers[x]);
+				if(x  < numbers.Length - 1)
+					Console.Write(", ");
+			}
+			Console.WriteLine($" are {result}.");
+		}
+		/// <summary>
+		/// Calculate the qouta from two integer and show the result in console window
+		/// </summary>
+		static void Division()
+		{
+			// Show a header for this division function
+			ShowAHeader("Division");
+
+			// Variables for calculation of qouta
+			int a, b;
+
+			// Ask the user for two integer
+			Console.Write("Write a number: ");
+			int.TryParse(Console.ReadLine(), out a);
+
+			Console.Write("Write a other number and it don't are zero: ");
+			int.TryParse( Console.ReadLine(), out b);
+
+			// Check if the 2:nd number are zero
+			if (b == 0)
+				Console.WriteLine("The secord number are zero and didn't allowed be that.");
+			else
+				Console.WriteLine($"The qouta from {a} and {b} are {a / b}.");
+		}
+		/// <summary>
+		/// Calculate the product from two integer and show the result in console window
+		/// </summary>
+		static void Multiplication()
+		{
+			// Show a header for this multiplication function
+			ShowAHeader("Multiplication");
+
+			// Variables for calculate the product
+			int a, b;
+
+			// Ask the user for two integer 
+			Console.Write("Write a number: ");
+			int.TryParse(Console.ReadLine(), out a);
+
+			Console.Write("Write a other number: ");
+			int.TryParse(Console.ReadLine(), out b);
+
+			// Calculate the product and show the result in console window
+			Console.WriteLine($"The product of {a} and {b} are {a * b}.");
+		}
 	}
 }
