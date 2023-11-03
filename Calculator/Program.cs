72bf89403ff5e0c4c6716f071490bee3c6761cdb
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2023-11-03 20:09:00 +0100

    Welcome the user to this program

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,11 +2,27 @@
 
 namespace Calculator
 {
+	/// <summary>
+	/// The class for this program
+	/// </summary>
 	internal class Program
 	{
+		/// <summary>
+		/// The name of this program
+		/// </summary>
+		public const string ProgramName = "The Calculator 2000";
+		
+		/// <summary>
+		/// The start point of this program
+		/// </summary>
+		/// <param name="args">Anything</param>
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// Welcome the user to this program
+			Console.WriteLine($"Welcome to {ProgramName}!");
+
+			// Make a pause in this program
+			Console.ReadKey();
 		}
 	}
 }
