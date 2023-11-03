bb16fcedcae3ff215c0c154fd42b578a45004749
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2023-11-03 21:50:17 +0100

    Add 1:st part of menu system for this program

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -18,12 +18,59 @@ namespace Calculator
 		/// <param name="args">Anything</param>
 		static void Main(string[] args)
 		{
+			// Variable for user choise
+			int choise;
+
 			// Welcome the user to this program
 			Console.WriteLine($"Welcome to {ProgramName}!");
 
 			// Make a pause in this program
 			Console.ReadKey();
+
+			do
+			{
+				// Show the menu of this program and wait for user make their chosie
+				int.TryParse(Menu(), out choise);
+
+				// Make a pause in this program
+				Console.ReadKey();
+
+			} while (choise != 99);
 		}
+		
+		/// <summary>
+		/// Show the menu of this program and wait for the users had make their choise
+		/// </summary>
+		/// <returns>The user choise</returns>
+		static string Menu()
+		{
+			// Create a empty console window, then show the name of this program
+			Console.Clear();
+            Console.WriteLine(ProgramName);
+
+            // Make a empty row
+            Console.WriteLine();
+
+			// Show a header of the menu
+			ShowAHeader("Menu");
+
+            // Make a empty row
+            Console.WriteLine();
+
+            // Show the menu itself
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+			Console.WriteLine("3. Division");
+            Console.WriteLine("4. Multiplication");
+
+            Console.WriteLine("99. Quit the program");
+
+            // Make a empty row
+            Console.WriteLine();
+
+			Console.Write("Make your choise: ");
+			return Console.ReadLine();
+        }
 		
 		/// <summary>
 		/// Show a header in console window
